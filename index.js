8d5f774d88fb7838da5517ac902c8747af3720a7
Ben Lubar <ben.lubar@gmail.com>
2016-08-25 11:33:39 -0500

    v1.0.3 - fix search term not being sent to Google after NodeBB update

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 var nconf = module.parent.require('nconf');
 
-function searchURL(term) {
-	return 'https://www.google.com/search?q=site:' + encodeURIComponent(nconf.get('url')) + '+' + encodeURIComponent(term);
+function searchURL(req) {
+	return 'https://www.google.com/search?q=site:' + encodeURIComponent(nconf.get('url')) + '+' + encodeURIComponent(req.params.term || req.query.term || '');
 }
 
 module.exports = {
 	"init": function(data, callback) {
 		data.router.route('/search/:term?').all(function(req, res, next) {
-			res.redirect(301, searchURL(req.params.term || ''));
+			res.redirect(301, searchURL(req));
 			res.end();
 		});
 		data.router.route('/api/search/:term?').all(function(req, res, next) {
-			res.status(308).json({external: searchURL(req.params.term || '')});
+			res.status(308).json({external: searchURL(req)});
 			res.end();
 		});
 
